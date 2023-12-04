678284fcbc534388a69c96114e815b0a873c3152
DavidCabrerizo <74828595+DavidCabrerizo@users.noreply.github.com>
2023-12-04 20:52:27 +0100

    Entrega final

--- script.js
+++ script.js
@@ -37,3 +37,53 @@ getCharacters(data => {
 
 })
 
+//Busqueda
+
+function getCharacters(done, searchTerm = "") {
+    let url = "https://rickandmortyapi.com/api/character";
+    
+    // Añadir término de búsqueda si se proporciona
+    if (searchTerm) {
+        url += `?name=${searchTerm}`;
+    }
+
+    const results = fetch(url);
+    results
+        .then(response => response.json())
+        .then(data => {
+            done(data);
+        })
+        .catch(error => {
+            console.log('Error: la API no se ha cargado bien', error);
+        });
+}
+
+function searchCharacters() {
+    const searchInput = document.getElementById("searchInput");
+    const searchTerm = searchInput.value.trim();
+
+    // Limpiar el contenido actual en el main
+    const main = document.querySelector("main");
+    main.innerHTML = "";
+
+    getCharacters(data => {
+        data.results.forEach(personaje => {
+            const article = document.createRange().createContextualFragment(/*html*/`
+                <article>
+                    <div class="image-container">
+                        <img src="${personaje.image}" alt="${personaje.name}">
+                    </div>
+                    <h2>${personaje.name}</h2>
+                    <span>${personaje.status}</span>
+                </article>
+            `);
+
+            main.append(article);
+        });
+    }, searchTerm);
+}
+
+// Cargar personajes al cargar la página
+searchCharacters();
+
+
